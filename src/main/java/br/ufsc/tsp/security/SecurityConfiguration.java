56e47135aa2369ed42245891ed9ab27d24cbe19e
Guilherme Sartori <guimsartori@gmail.com>
2021-10-02 20:27:43 -0300

    Added authentication filter

--- src/main/java/br/ufsc/tsp/security/SecurityConfiguration.java
+++ src/main/java/br/ufsc/tsp/security/SecurityConfiguration.java
@@ -1,13 +1,18 @@
 package br.ufsc.tsp.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.ufsc.tsp.security.filter.CustomAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -34,8 +39,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().anyRequest().permitAll();
+		http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 
 }
